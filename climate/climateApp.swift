ba949d8e7223b2cd7d2deaf6ada0a89f512f8cf4
xingsuchuan <sx80@duke.edu>
2022-06-06 22:00:55 -0400

    Initial Commit

---
+++ climate/climateApp.swift
@@ -0,0 +1,17 @@
+//
+//  climateApp.swift
+//  climate
+//
+//  Created by 邢苏川 on 6/6/22.
+//
+
+import SwiftUI
+
+@main
+struct climateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
